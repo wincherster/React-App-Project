973f728f9ae81701a4d21cb44340517598386507
wincherster <402992977@qq.com>
2019-09-26 08:48:09 +0800

    注册交互实现

--- src/components/authRoute/index.js
+++ src/components/authRoute/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 import axios from 'axios';
+
+// 引入router
+import { withRouter } from 'react-router-dom';
+@withRouter
 class AuthRoute extends React.Component {
   componentDidMount(){
+    const publicList = ['/login', '/register'];
+    console.log(this.props.location, 'props.location')
+    const pathname = this.props.location.pathname;
+    if(publicList.indexOf(pathname) > -1){
+      return null;
+    }
     /**
      *  获取用户信息 
      * 0. 判断是否需要登录，然后跳转登录
@@ -11,7 +21,17 @@ class AuthRoute extends React.Component {
      * */ 
     axios.get('/user/info')
     .then( res => {
-      console.log(res, 'userInfo')
+      if(res.data.code == 200){
+        // 判断登录信息
+        if(res.data.code == 0) {
+
+        }else {
+          console.log(this.props.history);
+
+          this.props.history.push('/login')
+        }
+      }
+      
 
     })
     .catch( err => console.log(err));
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -6,6 +6,9 @@ class Register extends React.Component {
   constructor(props){
     super();
     this.state = {
+      user: '',
+      pwd: '',
+      repeatpwd: '',
       type: 'genius'
     }
   }
@@ -21,9 +24,17 @@ class Register extends React.Component {
   }
 
   register() {
+    console.log('register Click', this.state)
     // 请求api
     
   }
+
+  handleChange(key, val) {
+    this.setState({
+      [key]: val
+    })
+  }
+
   render() {
     // 2019-09-05 使用方式
     const RadioItem = Radio.RadioItem;
@@ -33,11 +44,31 @@ class Register extends React.Component {
         <Logo></Logo>
         <div className="register-form">
         <List>
-            <InputItem placeholder="请输入用户名" clear>用户名</InputItem>
-            <InputItem placeholder="请输入密码" clear>密码</InputItem>
-            <InputItem placeholder="请再次输入密码" clear>确认密码</InputItem>
-            <RadioItem checked={ this.state.type === 'genius'} onClick={() => this.check('genius')}>牛人</RadioItem>
-            <RadioItem checked={ this.state.type === 'boss'} onClick={() => this.check('boss')}>BOSS</RadioItem>
+            <InputItem
+              onChange={
+                v=> this.handleChange('user', v)
+              }
+            placeholder="请输入用户名" clear>用户名</InputItem>
+            <InputItem 
+              onChange={
+                v=> this.handleChange('pwd', v)
+              }
+            placeholder="请输入密码" clear>密码</InputItem>
+            <InputItem 
+              onChange={
+                v=> this.handleChange('repeatpwd', v)
+              }
+            placeholder="请再次输入密码" clear>确认密码</InputItem>
+            <RadioItem 
+              onChange={
+                ()=> this.handleChange('type', 'genius')
+              }
+            checked={ this.state.type === 'genius'} onClick={() => this.check('genius')}>牛人</RadioItem>
+            <RadioItem 
+              onChange={
+                ()=> this.handleChange('type', 'boss')
+              }
+            checked={ this.state.type === 'boss'} onClick={() => this.check('boss')}>BOSS</RadioItem>
           </List>
           <WhiteSpace></WhiteSpace>
           <Button type="primary" onClick={() => this.register()}>注册</Button>
